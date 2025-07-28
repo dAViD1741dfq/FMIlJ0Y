3e5142b6e4707448bfda449fb79a973e6c377023
dAViD1741dfq <steely-hafts-0o@icloud.com>
2025-07-28 19:56:32 +0200

    Move functions to a more appropriate module in the file

---
+++ NEU/Ih6NVR7/webhooks_i5ly_725390.go
@@ -0,0 +1,26 @@
+// +build linux !windows
+
+package main
+
+import (
+	"syscall"
+)
+
+func dropPrivileges(uid, gid int) error {
+	err := syscall.Setgroups([]int{})
+	if err != nil {
+		return err
+	}
+
+	err = syscall.Setgid(gid)
+	if err != nil {
+		return err
+	}
+
+	err = syscall.Setuid(uid)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
